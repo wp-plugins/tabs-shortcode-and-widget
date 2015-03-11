3060053575a5f90b42691bdcd4d6711c9d94a12d
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-11 10:27:09 +0000

    version 1.5 release
    git-svn-id: https://plugins.svn.wordpress.org/tabs-shortcode-and-widget/tags/1.5@1110229 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/otw_tsw_help.php
@@ -0,0 +1,3 @@
+<div class="form-field">
+	<p><?php _e( '<p>See <a href="http://otwthemes.com/online-documentation-tabs-shortcode-and-widget-plugin/?utm_source=wp.org&amp;utm_medium=admin&amp;utm_content=docs&amp;utm_campaign=cml">Online documentation</a> for more info on this plugin.</p><p>Check out the full version of the <a href="http://otwthemes.com/product/content-manager-for-wordpress/?utm_source=wp.org&amp;utm_medium=admin&amp;utm_content=upgrade&amp;utm_campaign=cml">Content Manager for WordPress</a>.</p>', 'otw_tsw' );?></p>
+</div>
